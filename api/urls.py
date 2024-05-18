a2efaf0f9614720ba41d75f470cce718a15d17ec
jatmanis <133846606+jatmanis@users.noreply.github.com>
2024-05-18 12:46:00 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('even_odd', views.even_odd),
     path('test', views.test),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 """if settings.DEBUG:
     #urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)"""
+    """
