29670fb0a94908745786b758a34106adbca38022
jatmanis <133846606+jatmanis@users.noreply.github.com>
2024-05-18 12:42:33 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api import views
-"""from django.conf import settings
-from django.conf.urls.static import static"""
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example.urls')),
