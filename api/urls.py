f26b9d958f064602563d4adf7924f351839b4d07
jatmanis <133846606+jatmanis@users.noreply.github.com>
2024-05-18 12:39:57 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api import views
-from django.conf import settings
-from django.conf.urls.static import static
+"""from django.conf import settings
+from django.conf.urls.static import static"""
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example.urls')),
@@ -26,6 +26,6 @@ urlpatterns = [
     path('even_odd', views.even_odd),
     path('test', views.test),
 ]
-if settings.DEBUG:
+"""if settings.DEBUG:
     #urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)"""
